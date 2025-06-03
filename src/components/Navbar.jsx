8db16e94dea63bca851df20dbb44a45da9cb9f67
Nishma25 <nishmanalari@gmail.com>
2025-06-03 02:33:02 -0500

    padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex items-center py-5 fixed top-0 z-20 bg-background dark:bg-background-dark shadow-md">
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex justify-between items-center max-w-6xl mx-auto px-4 sm:px-8">
         <ScrollLink
           to="home"
           className="flex items-center gap-2"
